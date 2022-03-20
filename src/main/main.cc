607e662c83950fc07548af6fa288b7cbe6029f46
xwu36 <xwu36@buffalo.edu>
2022-03-19 22:39:45 -0700

    remove fully qualifier

--- src/main/main.cc
+++ src/main/main.cc
@@ -7,7 +7,7 @@ int main()
 {
   // Print Hello World.
   std::cout << "Hello World!" << std::endl;
-  ::fep::lib::Price4 p("12.34");
+  fep::lib::Price4 p("12.34");
   std::cout
       << "unscaled price of " << p.to_str() << " is " << p.unscaled() << std::endl;
   return 0;
